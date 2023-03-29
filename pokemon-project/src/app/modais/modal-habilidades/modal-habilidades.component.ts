44d4cc702e7fc3baa3585ec3eb4a44086e635204
victorleite06 <victor.leite2001@gmail.com>
2023-03-29 09:40:21 -0300

    Commit com intuito de testar as mudanças na vercel e início do modal de habilidades

--- pokemon-project/src/app/modais/modal-habilidades/modal-habilidades.component.ts
+++ pokemon-project/src/app/modais/modal-habilidades/modal-habilidades.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-modal-habilidades',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalHabilidadesComponent implements OnInit {
 
-  constructor() { }
+  detalhesHabilidades: any[] = []
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialog: MatDialogRef<ModalHabilidadesComponent>,
+    public pokemonService: PokemonService
+  ) { }
 
   ngOnInit(): void {
+    console.log(this.data)
+    this.buscarDetalhesHabilidades(this.data.abilities, 0)
   }
 
+  buscarDetalhesHabilidades(abilities: any, cont: number) {
+    this.pokemonService.requisicaoPorURL(abilities[cont].ability.url).subscribe({
+      next: res => {
+        this.detalhesHabilidades.push(res)
+        if(abilities[cont + 1]) {
+          this.buscarDetalhesHabilidades(abilities, cont + 1)
+        }
+        console.log(this.detalhesHabilidades)
+      }
+    })
+  }
+
+  tratarNome(name: string) {
+    return name.substring(0, 1).toUpperCase() + name.substring(1)
+  }
 }
