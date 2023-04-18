c19c52d4a6b792b9bebd8f7fb1c1211ca9b44899
victorleite06 <victor.leite2001@gmail.com>
2023-04-18 09:28:09 -0300

    Commit inicial do modal da área de encontro dos pokemons

--- pokemon-project/src/app/modais/modal-area-encontro/modal-area-encontro.component.ts
+++ pokemon-project/src/app/modais/modal-area-encontro/modal-area-encontro.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-modal-area-encontro',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalAreaEncontroComponent implements OnInit {
 
-  constructor() { }
+  detalhesEncontro: any[] = []
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialog: MatDialogRef<ModalAreaEncontroComponent>,
+    public pokemonService: PokemonService
+  ) { }
 
   ngOnInit(): void {
+    this.buscarDetalhesAreaEncontro()
   }
 
+  buscarDetalhesAreaEncontro() {
+    this.pokemonService.requisicaoPorURL(this.data.location_area_encounters).subscribe({
+      next: res => {
+        this.detalhesEncontro = res
+      }
+    })
+  }
+
+  tratarNome(nomeArea: string) {
+    while(nomeArea.includes('-')) {
+      nomeArea = nomeArea.replace('-', ' ')
+    }
+    nomeArea = nomeArea.replace('area', ' ')
+    return nomeArea.substring(0, 1).toUpperCase() + nomeArea.substring(1)
+  }
 }
